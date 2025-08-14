eff2a811451e04373c16fc5621e6ba5ea120bb6b
hyunjin-lee-2002 <hyunjinlee2002@gmail.com>
2025-08-14 18:17:59 +0900

    250814 18:17 이현진

--- src/main/java/com/javaex/controller/UserController.java
+++ src/main/java/com/javaex/controller/UserController.java
@@ -1,9 +1,34 @@
 package com.javaex.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(value="/user")
 public class UserController {
+	
+	//필드
+	
+	//메소드 일반
+	// --회원가입 폼
+	@RequestMapping(value = "/joinform", method = { RequestMethod.GET, RequestMethod.POST })
+	public String joinForm() {
+		System.out.println("UserController.joinFrom()");
+		
+		return "user/joinForm";
+	}
+	// --회원가입
+	
+	// --로그인 폼
+	
+	// --로그인
+	
+	// --로그아웃
+	
+	// --회원정보 수정폼
+	
+	
 	
 
 }
