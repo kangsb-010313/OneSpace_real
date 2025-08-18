8382a424bd08d516d211032ff7bf251ee9ca2383
nanom80 <nanomegi@gmail.com>
2025-08-18 18:30:17 +0900

    2025-08-18 18:30 신현석

--- src/main/java/com/javaex/controller/PracticeroomController.java
+++ src/main/java/com/javaex/controller/PracticeroomController.java
@@ -17,15 +17,16 @@ import com.javaex.vo.PracticeroomVO;
 public class PracticeroomController {
     private final PracticeroomService practiceroomService;
     public PracticeroomController(PracticeroomService s){ this.practiceroomService = s; }
-
+    
+    //http://localhost:8888/onespace/practice1_main
     // 최초 진입: 8장만 SSR
     @RequestMapping(value="/practice1_main", method={RequestMethod.GET, RequestMethod.POST})
     public String main(org.springframework.ui.Model model) {
         model.addAttribute("rooms", practiceroomService.getPagedList(0, 8));
         return "practiceroom/practice1_main";
     }
-
-    // 무한 스크롤용 API: 4장씩
+    
+    // 무한 스크롤용 API
     @GetMapping("/api/practicerooms")
     @ResponseBody
     public List<PracticeroomVO> listApi(@RequestParam(defaultValue="0") int page,
